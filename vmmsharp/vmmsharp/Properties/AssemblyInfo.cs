57f7e2b3c7076485642ce49532e426c366d942b7
Ulf Frisk <github_ufrisk@frizk.net>
2026-05-14 00:44:56 +0200

    Version 5.17.7

--- vmmsharp/vmmsharp/Properties/AssemblyInfo.cs
+++ vmmsharp/vmmsharp/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.Versioning;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.17.6.235")]
-[assembly: AssemblyFileVersion("5.17.6.235")]
+[assembly: AssemblyVersion("5.17.7.236")]
+[assembly: AssemblyFileVersion("5.17.7.236")]
